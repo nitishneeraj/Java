2bb6b3255df159ad7280551e0a7f828f4afccd3d
Nitish <nitishneeeraj16@gmail.com>
2022-06-22 08:30:02 +0530

    Linear Search,Num to word,Smallest/largestnumner,+ve/-ve number,checkPerfectSquare,Even/Odd Display,Sum od natural nuber

--- src/Number_Programs.java
+++ src/Number_Programs.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class Number_Programs {
 	static Scanner s1 = new Scanner(System.in);
-	static Number_Prog_all num = new Number_Prog_all();
+	static Number_Prog_Reverse num = new Number_Prog_Reverse();
+	static Number_to_Word num1  = new Number_to_Word();
+	static Number_programs_others  num_oth = new Number_programs_others();
 
 	static int scan() {
 		int num = s1.nextInt();
@@ -18,10 +20,16 @@ public class Number_Programs {
 		while (true) {
 			// Main Menu options
 			System.out.println("\n1.Reverse a Number");
-			System.out.println("2.Option 2");
-			System.out.println("3.Option 3");
-			System.out.println("4.Option 4");
-			System.out.println("5.Exit main menu");
+			System.out.println("2.Number to Word");
+			System.out.println("3.Sum of Natural Numbers");
+			System.out.println("4.Display Odd Numbers From 1 to 100");
+			System.out.println("5.Display Even Numbers From 1 to 100");
+			System.out.println("6.Perfect Square");
+			System.out.println("7.Positive or Negative");
+			System.out.println("8.Find Smallest of Three Numbers");
+			System.out.println("9.Largest of Three Numbers");
+			System.out.println("0.Exit main menu");
+			
 
 			System.out.print("Please enter your choice : ");
 			option1 = s1.nextInt();
@@ -33,21 +41,41 @@ public class Number_Programs {
 				break;
 			}
 			case 2:
-				// do something here
+				num1.Number_To_Word();
 				break;
 			case 3: {
-				
+				num_oth.Sum_of_Natural_Numbers();
 				break; // never forget to add this break statement
 			}
 			case 4:
+				num_oth.Display_Odd_Numbers();
 				break;
 			case 5:
+				num_oth.Display_Even_Numbers();
+				break;
+			case 6:
+				System.out.print("\nEnter a number: ");   
+				double number=s1.nextDouble();   
+				if (num_oth.checkPerfectSquare(number))   
+				System.out.println("Yes, the given number is perfect square.");  
+				else  
+				System.out.print("No, the given number is not perfect square.");    
+				break;				
+			case 7:
+				num_oth.Positive_or_Negative();
+				break;
+			case 8:
+				num_oth.SmallestNumber();
+				break;
+			case 9:
+				num_oth.Largest_of_Three_Numbers();
+				break;
+			case 0:
 				return; // terminate outer menu
 
 			default:
 				System.out.println("Invalid option");
 			}
 		}
-
 	}
 }
--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,18 @@ class Test {
 		Test t1 = new Test();
 		// t1.character();
 		// t1.missNumber();
-		math(9);
+		//math(9);
+		//test1();
+		t1.divide();
+	}
+	
+	
+	void divide() {
+		int a1=4,b1=4;
+		int b=a1 % 2 ;
+		int b2=b1 / 2 ;
+		System.out.println(b);
+		System.out.println(b2);
 	}
 
 	public static void missNumber() {
@@ -30,6 +41,27 @@ class Test {
 	public static void math(int num) {
 		double a = Math.sqrt(num);
 		System.out.println(a);
+	}
+	
+	public static void test1() {
+		int number =2;
+				int soFar=0;
+		String[] onedigit = { "", " One", " Two", " Three", " Four", " Five", " Six", " Seven",
+				" Eight", " Nine", " Ten", " Eleven", " Twelve", " Thirteen", " Fourteen", " Fifteen", " Sixteen",
+				" Seventeen", " Eighteen", " Nineteen" };
+		if (number % 100 < 20) {
+			soFar = (number % 100);
+			System.out.println("100"+soFar+"\n");
+			number = number / 100;
+		} else {
+			soFar = (number % 10);
+			//System.out.println("10"+soFar+"\n");
+			number = number / 10;
+			soFar = (number % 10) + soFar;
+			//System.out.println("101"+soFar+"\n");
+			number = number / 10;
+		}
+	//System.out.println(soFar);
 	}
 
 }
